3d57be2026b6eb6d81b53493936aa3ea90d8acf7
Ericson Tenório <contato.e72@gmail.com>
2026-04-01 18:50:28 -0300

    tualizar tabela

---
+++ inspect_status.py
@@ -0,0 +1,7 @@
+import urllib.request
+url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT_et0B2equYxZT7QXjReBl0VMJ1qx5clTNbnoXzULo1pKnWOkGh6wv3nAlgz9p-8gyKGDoVSXKEVSU/pub?gid=872949406&single=true&output=csv'
+with urllib.request.urlopen(url, timeout=20) as r:
+    text = r.read().decode('utf-8')
+for line in text.splitlines():
+    if line.startswith(',QD-10') and (',21,' in line or ',23,' in line):
+        print(line)
